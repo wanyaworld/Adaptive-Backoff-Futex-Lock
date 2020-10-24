ca7f3a4e0ab432f64364a01532218b34c9eeceb2
wanyaworld <6812skiii@gmail.com>
2020-10-24 17:25:23 +0900

    Init stop var

--- locks_test/bench.cpp
+++ locks_test/bench.cpp
@@ -4,6 +4,7 @@ struct bench *bench{};
 
 void init_bench(struct bench **bench, const unsigned int n_workers, const unsigned int duration) {
 	(*bench) = new struct bench;
+	(*bench)->stop = 0;
 	(*bench)->duration = duration;
 	(*bench)->n_workers = n_workers;
 	(*bench)->workers = new struct worker[n_workers];
